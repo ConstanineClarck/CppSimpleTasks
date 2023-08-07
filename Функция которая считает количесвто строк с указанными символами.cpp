f8ec921d7c2e8287638aeb7eed22e753f011c285
Constantine <141576338+ConstanineClarck@users.noreply.github.com>
2023-08-07 18:50:47 +0300

    Create Функция которая считает количесвто строк с указанными символами.cpp

---
+++ Функция которая считает количесвто строк с указанными символами.cpp
@@ -0,0 +1,46 @@
+/* Функция которая считает количесвто строк с указанными символами */
+
+#include <iostream>
+#include <cstring>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    
+    char str[70];
+    cout << "Введите строку.. ";
+    cin.getline(str, 70);
+    cout << endl;
+    
+    cout << "Введите символ слова с которым хотите подсчитать.. ";
+    char simbol = getchar();
+    
+    bool inWord = false;
+    bool Word = false;
+    
+    int NumberAlpha(0);
+    
+    
+    for(int i(0); str[i] != '\0'; i++)
+    {
+        if(!(inWord) && isalpha(str[i]))
+        {
+            inWord = true;
+            Word = false;
+        }
+        if(!(isalpha(str[i])))
+        {
+            inWord = false;
+        }
+        if((str[i] == simbol) && !Word)
+        {
+            Word = true;
+            NumberAlpha++;
+        }
+    }
+    
+    cout << NumberAlpha;
+    
+    return 0;
