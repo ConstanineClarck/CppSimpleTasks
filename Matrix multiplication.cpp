b213385d46fe943f34ad0e07d9303388a8835a7f
Constantine <141576338+ConstanineClarck@users.noreply.github.com>
2023-08-10 17:51:56 +0300

    Create Matrix multiplication.cpp

---
+++ Matrix multiplication.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    int RESULT = 0;
+    
+    
+    for(int i(0); i < N; i++)
+    {
+        for(int j()); j < L; j++)
+        {
+            
+        }
+    }
+    
+    for(int ii(0); ii < Q; ii++)
+    {
+        for(int jj(0); jj < U; jj++)
+        {
+            
+        }
+    }
+    
+    if(L != Q)
+    {
+        cout << "Количество столбцов первой матрицы не совпадают с колличесвтом строк второой матрицыэрицы'
+    }
+    
+    for(int i = 0; i < N; i++)
+    {
+        for(int j(0); j << L; j++)
+        {
+            for(int k(0); k < N; k++)
+            {
+                int result = matrix1[j][k] * matrix2[k][j];
+                RESULT += resul;
+                
+            }
+            
+            matrixRESULTAT[i][j] = RESULT;
+        }
+        
+        
+    }
+
+    return 0;
+}
